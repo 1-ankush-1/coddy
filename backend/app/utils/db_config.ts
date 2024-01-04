88d8a5ae40fcac46201ab75b31cd79db59fbe267
Ankush <ankushsharma9685@gmail.com>
2024-01-04 23:52:22 +0530

    db config for setup

---
+++ backend/app/utils/db_config.ts
@@ -0,0 +1,8 @@
+export default 
+{
+    database: process.env.DB,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    dialect: process.env.DIALECT
+}

1a21a300d44e4c9d6cceed6d75d69f3e1f402437
Ankush <ankushsharma9685@gmail.com>
2024-01-04 23:53:39 +0530

    added user model and its relation setup

---
+++ backend/app/models/user.ts
@@ -0,0 +1,35 @@
+import Sequelize from "sequelize";
+import sequelize from "../config/db_connect";
+
+const User = sequelize.define("user", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  phone: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  ispremiumuser: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false,
+    defaultValue: false,
+  }
+});
+
+export default User;

7bb6da92d20afc537e5d7870af2f95461082b59a
Ankush <ankushsharma9685@gmail.com>
2024-01-04 23:50:55 +0530

    created structure for auth controller

---
+++ backend/app/controllers/auth.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+const login = (req: express.Request, res: express.Response) => {};
+const signup = (req: express.Request, res: express.Response) => {};
+const forget = (req: express.Request, res: express.Response) => {};
+const reset = (req: express.Request, res: express.Response) => {};
+const update = (req: express.Request, res: express.Response) => {};
+
+export default {
+  login,
+  signup,
+  forget,
+  reset,
+  update,
+};
